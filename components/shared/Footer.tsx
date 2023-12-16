f39feb9bd78c04e682b949d7caab9456076e60a2
falak <falakgala09@gmail.com>
2023-12-16 17:13:41 +0530

    feat:footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,29 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <span className="ml-2">
+          Created By
+          <a
+            href="https://falak-gala.vercel.app/"
+            className="ml-2 underline text-blue-950">
+            Falak Gala
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
