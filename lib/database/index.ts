7e630af8ec9155c6616b01349a16be704d68a7f8
falak <falakgala09@gmail.com>
2023-12-17 18:18:53 +0530

    react hook form zod created event

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -9,12 +9,7 @@ export const connectToDatabase = async () => {
 
   if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URI, {
-      dbName: 'swift event',
-      bufferCommands: false,
-    });
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI);
 
   cached.conn = await cached.promise;
 
